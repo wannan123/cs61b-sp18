f7e7559462e1e68ac8467cd44e9095dbf001989f
Wu Chengen <98636764+wannan123@users.noreply.github.com>
2023-03-30 16:58:40 +0800

    fix the API

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,22 +1,16 @@
 package synthesizer;
 
-abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
 
+    @Override
     public int capacity() {
         return capacity;
     }
+
+    @Override
     public int fillCount() {
         return fillCount;
     }
-    public boolean isEmpty() {
-        return fillCount() == 0;
-    }
-    public boolean isFull() {
-        return capacity() == fillCount();
-    }
-    public abstract T peek();
-    public abstract T dequeue();
-    public abstract void enqueue(T x);
 }
