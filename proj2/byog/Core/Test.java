f0eb5b0bc4e51c983d1a03750801575189c969a6
‘wannan123’ <1397886153@qq.com>
2023-04-08 16:21:42 +0800

    add player and door

--- proj2/byog/Core/Test.java
+++ proj2/byog/Core/Test.java
@@ -1,10 +1,16 @@
 package byog.Core;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args){
         String temp = "N123S";
         long seed = Long.parseLong(temp.replaceAll("[^0-9]", ""));
-        System.out.println(seed);
+        Random random = new Random();
+        int x = random.nextInt(2,10);
+        int y = random.nextInt(2,10);
+        System.out.println(x);
+        System.out.println(y);
     }
 
 }
--- proj2/byog/Core/World.java
+++ proj2/byog/Core/World.java
@@ -23,7 +23,7 @@ public class World {
             count ++;
             int x = random.nextInt(Width - 1);
             int y = random.nextInt(Height - 1);
-            int width = random.nextInt(6) % (6 - 3 + 1) + 5;
+            int width = random.nextInt(6) % (6 - 3 + 1) + 6;
             int height = random.nextInt(6) % (6 - 3 + 1) + 5;
             boolean is_cross_border = CheckBoundary(x, y, width, height);
             if (is_cross_border && Room.checkExist(rooms,x,y,width,height)){
@@ -33,10 +33,22 @@ public class World {
                 break;
             }
         }
+
         System.out.println("Start draw");
         for (Room r : rooms){
             r.drawRoom(finalWorldFrame);
         }
+        /* generate the hallway */
+        for (int i = 0; i < rooms.size() - 1; i ++){
+            Room r = rooms.get(i);
+            int[][] res = HallWay.hallwayGenerator(r, rooms.get(i + 1));
+            HallWay.drawHallway2(finalWorldFrame, res);
+            HallWay.drawHallway1(finalWorldFrame, res);
+            HallWay.drawHallway(finalWorldFrame, res);
+
+        }
+        int[][] playerdoor = new int[2][2];
+        playerdoor = playDoor(finalWorldFrame);
 
     }
     public boolean CheckBoundary(int x, int y, int width, int height){
@@ -48,6 +60,37 @@ public class World {
             b = false;
         }
         return b;
+    }
+    public static boolean doorcheck(TETile[][] world, int x, int y) {
+        return (world[x][y] == Tileset.WALL);
+    }
+
+    public static boolean peoplecheck(TETile[][] world, int x, int y) {
+        return (world[x][y] == Tileset.FLOOR);
+    }
+    public int[][] playDoor(TETile[][] finalWorldFrame) {
+        int[][] results = new int[2][2];
+        int x0 = 0;
+        int y0 = 0;
+        int xDoor = 0;
+        int yDoor = 0;
+        while (!peoplecheck(finalWorldFrame, x0, y0)) {
+            x0 = random.nextInt(Width - 1);
+            y0 = random.nextInt(Height - 1);
+        }
+        while (!doorcheck(finalWorldFrame, xDoor, yDoor)) {
+            xDoor = random.nextInt(Width - 1);
+            yDoor = random.nextInt(Height - 1);
+        }
+
+        finalWorldFrame[x0][y0] = Tileset.PLAYER;
+        finalWorldFrame[xDoor][yDoor] = Tileset.LOCKED_DOOR;
+
+        results[0][0] = x0;
+        results[0][1] = y0;
+        results[1][0] = xDoor;
+        results[1][1] = yDoor;
+        return results;
     }
 
 }
