4fd69797600adbd52c0860cf7007052d23aa1507
‘wannan123’ <1397886153@qq.com>
2023-05-02 20:44:28 +0800

    create lab8

---
+++ lab8/MyHashMap.java
@@ -0,0 +1,56 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by JunhaoW on 05/14/2019
+ */
+
+public class MyHashMap<K, V> implements Map61B<K, V>{
+    public int initialSize = 16;
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
