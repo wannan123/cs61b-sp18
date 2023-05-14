a31fffeed9ff8366ba8614a2a3ab9f13d2632bac
‘wannan123’ <1397886153@qq.com>
2023-05-14 15:32:02 +0800

    finish breadthFirstPATHS

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,72 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> queue;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        queue = new ArrayDeque<Integer>(maze.V());
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s] = true;
+        announce();
+        if (v == t) {
+            targetFound = true;
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            queue.add(w);
+            edgeTo[w] = v;
+            marked[w] = true;
+            announce();
+            distTo[w] = distTo[v] + 1;
+            if (w == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+        }
+
+        while (!queue.isEmpty()){
+            int pop = queue.remove();
+            for (int w : maze.adj(pop)) {
+                if (!marked[w]) {
+                    queue.add(w);
+                    edgeTo[w] = pop;
+                    marked[w] = true;
+                    distTo[w] = distTo[pop] + 1;
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                    }
+                    if (targetFound){
+                        return;
+                    }
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
