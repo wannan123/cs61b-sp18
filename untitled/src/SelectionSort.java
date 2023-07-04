f28eaeafc70b05d1c5e5b2aca62e0854442ac20f
‘wannan123’ <1397886153@qq.com>
2023-07-04 16:33:39 +0800

    add

---
+++ untitled/src/SelectionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr)); // [11, 12, 22, 25, 64]
+    }
+}
